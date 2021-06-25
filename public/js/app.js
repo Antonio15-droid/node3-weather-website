d1fcdcce0cf48af4d8b5a18a0496c36c8a69068a
Antonio Agustin Alviz <alvizantonioagustin15@gmail.com>
2021-06-25 11:07:34 +0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit', (e) =>{
     e.preventDefault()
